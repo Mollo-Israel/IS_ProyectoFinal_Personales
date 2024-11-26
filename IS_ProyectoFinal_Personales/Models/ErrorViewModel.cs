6e4d9afbf318b296f600d866040151e6a29e4d8a
Mollo-Israel <mvi0032517@est.univalle.edu>
2024-11-26 14:30:28 -0400

    First Commit

---
+++ IS_ProyectoFinal_Personales/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IS_ProyectoFinal_Personales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
